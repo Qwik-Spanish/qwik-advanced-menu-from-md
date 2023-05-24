e0e375c872d823a28deb37bcf0016ada756a3686
anartzdev <105705996+anartzdev@users.noreply.github.com>
2023-05-24 07:34:59 +0200

    feat: two level menu finish

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,43 +1,44 @@
-import { component$, Slot, useStyles$, useTask$ } from "@builder.io/qwik";
-import { useContent } from "@builder.io/qwik-city";
-
+import { component$, Slot, useStyles$ } from "@builder.io/qwik";
+// 1 - Añadir ContentMenu
+import { ContentMenu, useContent } from "@builder.io/qwik-city";
+import menuStyles from "./menu.css?inline";
 export default component$(() => {
-  useStyles$(`
-  .menu {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-  }
+  useStyles$(menuStyles);
   
-  .menu li {
-    display: inline-block;
-    margin-right: 10px;
-  }
-  
-  .menu li a {
-    text-decoration: none;
-    color: #000;
-    padding: 5px 10px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-  }
-  
-  .menu li a:hover {
-    background-color: #f0f0f0;
-  }
-  `)
   const { menu } = useContent();
+
+  // 2.- Para definir si un enlace es interno /externo
+  const isLocalUrl = (url: string) => {
+    return url.startsWith('http://') || url.startsWith('https://') ? '_blank' : '_self'
+  };
+  
+  // 3.- Estructura del menú
   return (
     <>
-      <ul class="menu">
-        {menu &&
-          menu.items &&
-          menu.items.map((item) => (
-            <li>
-              <a href={item.href}>{item.text}</a>
-            </li>
-          ))}
+      <ul id="menu">
+        {menu?.items?.map((item: ContentMenu) => {
+          if (item?.items?.length === 1) { // Menús sin hijos
+            return (
+              <li class="parent">
+                <a href={item.items[0].href} target={isLocalUrl(item.items[0].href || '')}>{item.text}</a>
+              </li>
+            );
+          }
+          return <li class="parent">
+              <a href="#">{item.text}</a>
+              <ul class="child">
+                {
+                  item?.items?.map((element: ContentMenu) => (
+                    <li>
+                      <a href={element.href || ''} target={isLocalUrl(element.href || '')}>{element.text || ''}</a>
+                    </li>
+                  ))
+                }
+              </ul>
+            </li>;
+        })}
       </ul>
+      <br />
       <Slot />
     </>
   );
--- src/routes/menu.md
+++ src/routes/menu.md
@@ -1,5 +1,25 @@
 # Menú Principal
 
-- [Inicio](/)
-- [Proyectos](projects/index.md)
-- [Contacto](contact/index.md)
+## Inicio
+
+- [-](/)
+
+## Proyectos
+
+- [-](/projects)
+
+## Qwik Oficial
+
+- [Documentación](https://qwik.builder.io/docs/)
+- [Playground](https://qwik.builder.io/examples/)
+- [Curso GRATIS](https://medium.com/@mugan86/list/qwik-paso-a-paso-desde-0-al-detalle-e7df8b471166)
+- [Qwik en Español - Ayuda](https://github.com/Qwik-Spanish/qwik-i18n-translate-es)
+
+## Componentes
+
+- [Inicio](https://qwik.builder.io/docs/components/overview/)
+- [Tareas y Ciclos de Vida](https://qwik.builder.io/docs/components/tasks/#tasks)
+  
+## Contacto
+
+- [-](/contact)
